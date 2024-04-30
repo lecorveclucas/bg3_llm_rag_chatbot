9510a9c5fed7aa9cf4f31932f195ae4af2db0c51
Lucas <lecorvec.lucas@gmail.com>
2024-04-30 16:59:58 +0200

    Tried various techniques to increase model's reponses : added preprompt, tried to add memory, etc.

--- chatbot/main.py
+++ chatbot/main.py
@@ -5,7 +5,16 @@
 # Build a scrapper to scrap data from BG3
 # Build a web app (through chatgpt4)
 # Create agent depending on which db to use ? character, weapons, skills, etc. ?
-# Add a preprompt
+# Search to determine how to evaluate this kind of rag
+# Plot a graph if tokens distribution in sentences  ==> 90% of paragraphs have less than 530 tokens and less than 390 words
+
+# Model tuning 
+# 1. Too much documents from retriever seems to make it hallucinate
+# 2. The pre prompt help really much to avoid hallucinating and too be factual ==> Preprompt has a negative impacts on model's reponse as it embeds the preprompt and so it impacts the semantic search in the RAG
+# 3. The history might distrub the model answer, to investigate
+# 4. Try to divide by chunks, embed only the question THEN give the context to a prepromt
+# 5. Names are not embedded, resulting in confusion in match hit with the RAG
+# 6. Try hybrid search (with keywords retriever)
 
 import os
 import streamlit as st
@@ -15,10 +24,12 @@ from llama_index.llms.llama_cpp import LlamaCPP
 from llama_index.llms.llama_cpp.llama_utils import messages_to_prompt, completion_to_prompt
 from llama_index.core.readers import SimpleDirectoryReader
 from llama_index.core import Document
-from llama_index.core.node_parser import SentenceWindowNodeParser
+from llama_index.core.node_parser import SentenceWindowNodeParser, SentenceSplitter
 from llama_index.core import VectorStoreIndex, StorageContext, load_index_from_storage
 from llama_index.core import Settings
 from llama_index.core.postprocessor import MetadataReplacementPostProcessor, SentenceTransformerRerank
+
+text_color = "white"
 
 def get_base64(bin_file):
     with open(bin_file, 'rb') as f:
@@ -52,12 +63,11 @@ llm = LlamaCPP(
     # optionally, you can set the path to a pre-downloaded model instead of model_url
     model_path=None,
     temperature=0.1,
-    max_new_tokens=256,
+    max_new_tokens=512,
     # llama2 has a context window of 4096 tokens, but we set it lower to allow for some wiggle room
     context_window=4096,
     # kwargs to pass to __call__()
     generate_kwargs={},
-    # kwargs to pass to __init__()
     # set to at least 1 to use GPU
     model_kwargs={"n_gpu_layers": 15},
     # transform inputs into Llama2 format
@@ -68,15 +78,17 @@ llm = LlamaCPP(
 
 def get_build_index(documents, llm, embed_model="local:BAAI/bge-small-en-v1.5", sentence_window_size=5,
                     save_dir="./vector_store/index"):
-    node_parser = SentenceWindowNodeParser(
+    sentence_window_parser = SentenceWindowNodeParser(
         window_size=sentence_window_size,
         window_metadata_key="window",
         original_text_metadata_key="original_text"
     )
 
+    paragraph_parser = 0 # To implement
+
     Settings.llm = llm
     Settings.embed_model = embed_model
-    Settings.node_parser = node_parser
+    Settings.node_parser = sentence_window_parser
 
     if not os.path.exists(save_dir):
         # create and load the index
@@ -89,7 +101,7 @@ def get_build_index(documents, llm, embed_model="local:BAAI/bge-small-en-v1.5", 
 
     return index
 
-def get_query_engine(sentence_index, similarity_top_k=6, rerank_top_n=2):
+def get_query_engine(sentence_index, similarity_top_k=3, rerank_top_n=2):
     postproc = MetadataReplacementPostProcessor(target_metadata_key="window")
     rerank = SentenceTransformerRerank(
         top_n=rerank_top_n, model="BAAI/bge-reranker-base", device="mps"
@@ -100,8 +112,16 @@ def get_query_engine(sentence_index, similarity_top_k=6, rerank_top_n=2):
 
     return engine
 
+def queries_memory(session_state: st.session_state, pre_prompt: str):
+    messages_history = session_state.messages
+    if len(messages_history) > 2:
+        pre_prompt += f"User: {messages_history[-3]["content"]}\n"
+        pre_prompt += f"Assistant: {messages_history[-2]["content"]}\n"
+    pre_prompt += f"Question: {st.session_state.messages[-1]["content"]}"
+    return pre_prompt
 
-st.markdown('<style>.chat-input { background-color: black; }</style>', unsafe_allow_html=True)
+
+# st.markdown('<style>.chat-input { background-color: grey; }</style>', unsafe_allow_html=True)
 
 st.header("Chat with the Baldur's Gate 3 wiki 🎲 🎮")
 
@@ -113,8 +133,8 @@ if "messages" not in st.session_state.keys(): # Initialize the chat message hist
 @st.cache_resource(show_spinner=False)
 def load_db():
     with st.spinner(text="Loading and indexing the Streamlit docs – hang tight! This should take 1-2 minutes."):
-        quests_files = glob.glob("./data/quests/*.txt")
-        documents = SimpleDirectoryReader(input_files=quests_files).load_data()
+        files = glob.glob("./data/*/*.txt")
+        documents = SimpleDirectoryReader(input_files=files).load_data()
         documents = Document(text="\n\n".join([doc.text for doc in documents]))
 
         # Get the vector index
@@ -125,21 +145,37 @@ def load_db():
 
 vector_index = load_db()
 
-query_engine = get_query_engine(sentence_index=vector_index, similarity_top_k=6, rerank_top_n=2)
+query_engine = get_query_engine(sentence_index=vector_index, similarity_top_k=3, rerank_top_n=2)
+
+# Preprompt
+pre_prompt = """<s>[INST] Your job is to use information about the game 
+Baldur's Gate 3 (BG3) in order to answer questions. Use the given
+context to answer questions. Be as detailed as possible, but 
+don't make up any information that's not from the context. 
+If you don't know an answer, say you don't know.</s>
+
+[INST]
+Question : {question}
+Context : 
+[/INST]
+"""
 
 if prompt := st.chat_input("Your question"): # Prompt for user input and save to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
 
 for message in st.session_state.messages: # Display the prior chat messages
     with st.chat_message(message["role"]):
-        st.write(f":black[{message['content']}]")
+        st.write(f":{text_color}[{message['content']}]")
 
 # If last message is not from assistant, generate a new response
 if st.session_state.messages[-1]["role"] != "assistant":
     with st.chat_message("assistant"):
         with st.spinner("Thinking..."):
-            response = query_engine.query(st.session_state.messages[-1]["content"])
-            st.write(response.response)
+            # context = queries_memory(session_state=st.session_state, pre_prompt=pre_prompt)
+            question = st.session_state.messages[-1]["content"]
+            # response = query_engine.query(pre_prompt.format(question=question))
+            response = query_engine.query(question)
+            st.write(f":{text_color}[{response.response}]")
             message = {"role": "assistant", "content": response.response}
             st.session_state.messages.append(message) # Add response to message history
 
