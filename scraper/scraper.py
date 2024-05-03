8e01d0886bd12823e57b69db986c3f31278c7b63
Lucas <lecorvec.lucas@gmail.com>
2024-05-03 18:57:41 +0200

    Added security mecanisme to avoid scrapping page that have not been found

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -34,6 +34,10 @@ def extract_content(url: str, output_file_path: str, classes_to_parse: list):
         # Check if the next element is of class "bonfire" or "titlearea"
         if 'class' in sibling.attrs and sibling.attrs['class'][0] in classes_to_parse:
             break
+
+    # Check if page has been found
+    if "page not found" in page_description.lower():
+        return None
         
     # Find all elements with class 'titlearea' or 'bonfire'
     extracted_text = page_description + '\n\n'
@@ -114,7 +118,56 @@ def process_folder(folder_name: str, classes_to_parse: list):
         extract_content(url=name_url, output_file_path=os.path.join(output_folder, output_file_name), classes_to_parse=classes_to_parse)
 
 # Process quests
-process_folder(folder_name="quests", classes_to_parse=["titlearea", "bonfire"])
+# process_folder(folder_name="quests", classes_to_parse=["titlearea", "bonfire"])
 
 # Process companions
-process_folder(folder_name="companions", classes_to_parse=["titlearea", "bonfire", "special"])
+# process_folder(folder_name="companions", classes_to_parse=["titlearea", "bonfire", "special"])
+
+# Process races
+# process_folder(folder_name="races", classes_to_parse=["titlearea", "bonfire", "special"])
+
+# Process bosses
+# process_folder(folder_name="bosses", classes_to_parse=["titlearea", "bonfire", "special"])
+
+# Process npcs
+# process_folder(folder_name="npcs", classes_to_parse=["titlearea", "bonfire", "special"])
+
+
+
+
+import os
+import streamlit as st
+import glob
+import base64
+import pandas as pd
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from llama_index.llms.llama_cpp import LlamaCPP
+from llama_index.llms.llama_cpp.llama_utils import messages_to_prompt, completion_to_prompt
+from llama_index.core.readers import SimpleDirectoryReader
+from llama_index.core.node_parser import SentenceWindowNodeParser, SentenceSplitter, SemanticSplitterNodeParser, TokenTextSplitter
+from llama_index.core import Document, VectorStoreIndex, StorageContext, load_index_from_storage, Settings
+from llama_index.core.schema import TextNode
+from llama_index.core.postprocessor import MetadataReplacementPostProcessor, SentenceTransformerRerank
+from llama_index.core.evaluation import generate_question_context_pairs, RetrieverEvaluator
+from transformers import AutoTokenizer
+
+
+llm = LlamaCPP(
+    # You can pass in the URL to a GGML model to download it automatically
+    # model_url='https://huggingface.co/TheBloke/Mixtral-8x7B-v0.1-GGUF/resolve/main/mixtral-8x7b-v0.1.Q4_K_M.gguf',
+    model_url='https://huggingface.co/TheBloke/Mistral-7B-Instruct-v0.2-GGUF/resolve/main/mistral-7b-instruct-v0.2.Q4_K_M.gguf',  # Q6_K was used too but quite slow
+    # optionally, you can set the path to a pre-downloaded model instead of model_url
+    model_path=None,
+    temperature=0.1,
+    max_new_tokens=512,
+    # Context size
+    context_window=8192, # Had to increase form 4096 to 8192 because the evaluation create 4600 tokens prompt
+    # kwargs to pass to __call__()
+    generate_kwargs={},
+    # set to at least 1 to use GPU
+    model_kwargs={"n_gpu_layers": 10},
+    # transform inputs into Llama2 format
+    messages_to_prompt=messages_to_prompt,
+    completion_to_prompt=completion_to_prompt,
+    verbose=True
+)
